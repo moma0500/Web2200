93946c7af2c4aaab99a95c2d76942d625f7cfbc5
moma0500 <89860959+moma0500@users.noreply.github.com>
2021-12-10 21:44:27 -0700

    background edits

--- Final_Project/sketch.js
+++ Final_Project/sketch.js
@@ -2,11 +2,12 @@
 var canvas;
 var timer;
 var timer2;
-var counter = 0;
+var counter;
 var interval;
 var button;
+var secondscounter = 10;
 
-let amount = 30;
+let amount = 50;
 let points = [];
 let maxDistance = 250;
 let speed = .001;
@@ -19,13 +20,47 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
-  timer = createP('timer');
-  timer2 = createP('timer 2')
-  button = createButton('start timer');
-  button.mousePressed(doTimer);
+  resetSketch();
+  // timer = createP('timer');
+  // timer2 = createP('timer 2');
+
+  // makeTimer();
+
+  // button = createButton('start timer');
+  // button.mousePressed(doTimer);
   // interval = setInterval(timeIt,500);
 
+  // function makeTimer(){
+  //   var counter = 0;
+  //   setInterval(timeIt, 500);
+  //
+  //   function timeIt() {
+  //     timer.html(counter);
+  //     timer2.html(counter);
+  //     counter++;
+  //   }
+  // }
 
+  // for (let i = 0; i < amount; i++) {
+  //   points[i] = {
+  //     x: random(width),
+  //     y: random(height)
+  //   };
+  //   ellipse(points[i].x, points[i].y);
+  // }
+
+  var sketchbutton = createButton("reset");
+  sketchbutton.mousePressed(resetSketch);
+  sketchbutton.id = 'sketchbutton';
+
+
+  // for (let i = 0; i < secondscounter; i++) {
+  //   resetSketch();
+  // }
+
+}
+
+function resetSketch() {
   for (let i = 0; i < amount; i++) {
     points[i] = {
       x: random(width),
@@ -33,23 +68,17 @@ function setup() {
     };
     ellipse(points[i].x, points[i].y);
   }
-
 }
 
-function doTimer() {
-  interval = setInterval(timeIt, 500);
-  button.html('stop timer');
-}
+// function doTimer() {
+//   interval = setInterval(timeIt, 500);
+//   button.html('stop timer');
+// }
 
 // function stopTimer() {
 //   clearInterval(interval);
 // }
 
-function timeIt() {
-  timer.html(counter);
-  timer2.html(counter);
-  counter++;
-}
 
 function draw() {
 
@@ -62,7 +91,7 @@ function draw() {
       let d = dist(points[i].x, points[i].y,
         points[j].x, points[j].y);
 
-      let mapD = map(d, -100, maxDistance, 4000, 50);
+      let mapD = map(d, -100, maxDistance, 4000, 0);
 
       stroke(0, mapD);
       // stroke('#FFCE34');
@@ -75,8 +104,8 @@ function draw() {
 
   }
   for (let i = 0; i < amount; i++) {
-    points[i].x += (noise(i + 100 + frameCount * speed) - .49) * 3;
-    points[i].y += (noise(i + frameCount * speed) - .49) * 3;
+    points[i].x += (noise(i + 100 + frameCount * speed) - .49) * 1;
+    points[i].y += (noise(i + frameCount * speed) - .49) * 1;
   }
 
 
